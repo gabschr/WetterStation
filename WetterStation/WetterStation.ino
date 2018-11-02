97128c1b819828ffb903c7a06f4dabe134e57e87
Gabriel <>
2018-11-02 21:37:48 +0100

    Projektdateien hinzufügen.

---
+++ WetterStation/WetterStation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WetterStation.ino
+ Created:	02.11.2018 21:37:36
+ Author:	GS
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
