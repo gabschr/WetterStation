98ee737fc6681a6b46c81bdc9a246ed7d2949ac1
Gabriel <gabschr@gmx.de>
2018-11-02 22:39:45 +0100

    Auf GitHub speichern

--- WetterStation/WetterStation.ino
+++ WetterStation/WetterStation.ino
@@ -4,6 +4,8 @@
  Author:	HN und GS
 */
 
+
+
 // #include <dht11.h>
 
 #define ledDDR   DDRB  // Port Group B DDR - als Ausgang oder Eingang definieren
@@ -18,14 +20,10 @@
 #define FeuchtLuftPD    PD3    // PD für Feuchtigkeitssensor
 #define FeuchtLuftPIN   PIND   // PIN GROUP D
 
-// Bibliothek zum Auslesen 
-#define DHT11PIN 3
-dht11 DHT11;
+ //Bibliothek zum Auslesen 
+ //#define DHT11PIN 3
+ //dht11 DHT11;
 
-/*
-  taster = (0 << PD2); //als Eingang definieren, Pin ist PortD, PIN 2
-  ledPORT = (1 << PB5); //als Ausgang definieren, PIN ist PortB, PIN 5 (Digital 13)
-*/
 
 bool buttonState = 0;  //Tasterstatus feststellen
 int counter = 0;
@@ -50,7 +48,6 @@ void setup() {
 	Serial.println("Ausgabe am Monitor");
 	Serial.println("------------------");
 	// digitalWrite(ledPORT, LOW);
-
 }
 
 void loop() {
@@ -80,53 +77,19 @@ void loop() {
 		}
 	}
 
+	
 	/*
-	  // Temp und Feuchtigkeit abfragen
-
-	  // Sensor aktivieren
-	  FeuchtLuftDDR = (1 << FeuchtLuftPD);   // als Ausgang setzen
-	  FeuchtLuftPORT = (0 << FeuchtLuftPD);  // auf LOW setzen
-	  delay(18);
-
-	  FeuchtLuftPORT = (1 << FeuchtLuftPD);            // auf HIGH setzen
-	  delayMicroseconds(40);
-
-	  FeuchtLuftDDR = (0 << FeuchtLuftPIN);   // als Eingang setzen
-
-	  // Response abwarten
-	  for (int i=0; i < 2; i++){
-		delayMicroseconds(40);
-		Serial.print("Feuchtigkeitssensor aktiv, FeuchtLuft = ");
-		Serial.print(FeuchtLuft);
-		Serial.print(", FeuchtLuftVorher ");
-		Serial.println(FeuchtLuftVorher);
-		FeuchtLuft = (FeuchtLuftPIN & (1 << FeuchtLuftPD));
-		if (FeuchtLuftVorher == FeuchtLuft) {
-		   FeuchtLuft = 99;
-		   break;
-		}
-		if (FeuchtLuftVorher != FeuchtLuft){
-		  if (FeuchtLuft == 1){
-			FeuchtLuftVorher == 1;
-		  }
-		  if (FeuchtLuft == 0){
-			FeuchtLuftVorher == 0;
-		  }
-		}
-		delayMicroseconds(40);
-	  }
-	  */
-
 	  //Feuchtigkeitssensor auslesen mit Bibliothek
 	int chk = DHT11.read(DHT11PIN);
 	Serial.print("Temperatur: ");
 	Serial.print((float)DHT11.temperature, 2);
 	Serial.print(", Feuchtigkeit: ");
 	Serial.println(DHT11.humidity, 2);
+	*/
 
 	delay(3600);
 
-
+	// fröhlich blinken lassen :)
 	FeuchtLuft = 0;
 
 	for (FeuchtLuft = 1; FeuchtLuft <= 10; FeuchtLuft++) {
